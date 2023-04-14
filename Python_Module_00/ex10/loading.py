6a5bfbd9a93f264cc017d528030d56b17aeafa61
Jihoon Lee <brain2400@gmail.com>
2023-04-14 18:11:55 +0900

    [ADD]: ex00 added

--- Python_Module_00/ex10/loading.py
+++ Python_Module_00/ex10/loading.py
@@ -6,19 +6,7 @@
 #    By: jihoolee <jihoolee@student.42SEOUL.kr>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/09 16:51:26 by jihoolee          #+#    #+#              #
-#    Updated: 2023/04/09 16:52:18 by jihoolee         ###   ########.fr        #
+#    Updated: 2023/04/14 18:11:33 by jihoolee         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import sys
-
-
-def main(argv):
-    pass
-
-
-if (__name__ == '__main__'):
-    try:
-        main(sys.argv)
-    except Exception as e:
-        print('ERROR')
